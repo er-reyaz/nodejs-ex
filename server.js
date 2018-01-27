836a5c307982ea31c10e7d3f4dc977603ca9c3b0
Reyaz Ahmad <er-reyaz@users.noreply.github.com>
2018-01-27 21:40:20 +0530

    update tasks

--- server.js
+++ server.js
@@ -2,16 +2,19 @@
 var express = require('express'),
     app     = express(),
     morgan  = require('morgan');
+var bodyParser = require('body-parser');
+var tasks = require('./tasks');
     
 Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile); 
 app.use(morgan('combined'));
-var tasks = require('./tasks');
 app.use('/api', tasks);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
 
