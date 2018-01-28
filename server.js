bc9758f78b8f7d0c6e65e8f4d7239435b164c453
Reyaz Ahmad <er-reyaz@users.noreply.github.com>
2018-01-28 10:59:35 +0530

    update tasks

--- server.js
+++ server.js
@@ -6,6 +6,20 @@ var bodyParser = require('body-parser');
 var tasks = require('./tasks');
     
 Object.assign=require('object-assign')
+// Add headers to solve CORS problem
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  // Pass to next layer of middleware
+  next();
+});
 
 app.engine('html', require('ejs').renderFile); 
 app.use(morgan('combined'));
--- tasks.js
+++ tasks.js
@@ -2,13 +2,46 @@ var express = require('express');
 var router = express.Router();
 var mongojs = require('mongojs');
 // var db = mongojs('mongodb://reyaz:reyaz@ds139979.mlab.com:39979/tasklist', ['tasks']);//cloud
- var db = mongojs('mongodb://reyaz:reyaz@ds235807.mlab.com:35807/nse', ['virtual']);
+ var db = mongojs('mongodb://reyaz:reyaz@ds235807.mlab.com:35807/nse'); // 'actual,virtual,profit,nselist'
 // mongodb://<dbuser>:<dbpassword>@ds139979.mlab.com:39979/tasklist
 //var db = mongojs('mongodb://127.0.0.1:27017/tasklist', ['tasks']);//local
 
-// Get All Tasks
+// Get All Virtual
 router.get('/virtual', function(req, res, next){
     db.virtual.find(function(err, table){
+        if(err){
+            res.send(err);
+        }else {
+            res.json(table);
+        }
+    });
+});
+
+// Get All Actual
+router.get('/actual', function(req, res, next){
+    db.actual.find(function(err, table){
+        if(err){
+            res.send(err);
+        }else {
+            res.json(table);
+        }
+    });
+});
+
+// Get All profit
+router.get('/profit', function(req, res, next){
+    db.profit.find(function(err, table){
+        if(err){
+            res.send(err);
+        }else {
+            res.json(table);
+        }
+    });
+});
+
+// Get All nselist
+router.get('/nselist', function(req, res, next){
+    db.nselist.find(function(err, table){
         if(err){
             res.send(err);
         }else {
